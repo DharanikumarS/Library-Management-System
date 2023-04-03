dc1eeaeb77f8cda9ed80b5264e61cebd615f1d79
DharanikumarS <dharanikumars23@gmail.com>
2023-04-03 10:48:13 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./styles/App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import AdminLogin from "./components/AdminLogin";
+import UserLogin from "./components/UserLogin";
+
+const App = () => {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/adminLogin" element={<AdminLogin />} />
+          <Route path="/user-login" element={<UserLogin />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
